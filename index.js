686bd95f88a47c63af2fa031fe9929475d2ded7c
Prasad Bhalerao <prasad9a38@gmail.com>
2026-05-12 20:17:17 +0530

    Implement socket connection for real-time location updates

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ async function main() {
 
     io.attach(server);
 
+    io.on("connection", (socket) => {
+        console.log(`[Socket:${socket.id}] connected!`);
+
+        socket.on("client:location:update", (locationData) => {
+            const { latitude, longitude } = locationData;
+            console.log(
+                `[Socket:${socket.id}]:client:location:update`,
+                locationData,
+            );
+        });
+    });
+
     app.use(express.static(path.resolve("./public")));
 
     app.get("/health", (req, res) => {
